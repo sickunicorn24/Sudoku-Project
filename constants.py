26d369454c97478b96e535491af8d7e62d07d95c
Derek Camilo <djcamilo2003@gmail.com>
2024-11-24 21:53:41 -0500

    added a constants file

---
+++ constants.py
@@ -0,0 +1,10 @@
+#All of these values are subject to change
+
+WIDTH = 540
+HEIGHT = 600
+BG_COLOR_1 = (255,255, 255) #White
+BG_COLOR_2 = (150, 10, 255) #Purple *IF NECCESSARY*
+TEXT = (0, 0, 0) #Black
+TITLE_TEXT = (255, 10, 200) #Pink
+BUTTON_COLOR = (255, 10, 200) #Pink
+BUTTON_TEXT = (255,255, 255)
---
+++ sudoku.py
@@ -0,0 +1,54 @@
+import pygame, sys
+from constants import *
+from sudoku_generator import *
+
+def game_start(screen):
+    #Title/Button font init
+    start_title_font = pygame.font.Font(None, 100)
+    button_font = pygame.font.Font(None, 70)
+
+    #Background coloring
+    screen.fill(BG_COLOR_1)
+
+    #Creating title text
+    title_surface = start_title_font.render("Welcome to Sudoku", 0, TITLE_TEXT)
+    title_rectangle = title_surface.get_rect(center = (WIDTH // 2, HEIGHT // 2 - 150))
+    screen.blit(title_surface, title_rectangle)
+
+    #Init Button
+    start_text = button_font.render("Start", 0, BUTTON_TEXT)
+    quit_text = button_font.render("Quit", 0, BUTTON_TEXT)
+
+    #Init START/QUIT Button Text Box Background
+    start_surface = pygame.Surface((start_text.get_size()[0] * 20, start_text.get_size()[1] * 20))
+    start_surface.fill(BUTTON_COLOR)
+    start_surface.blit(start_text, (10, 10))
+    quit_surface = pygame.Surface((quit_text.get_size()[0] * 20, quit_text.get_size()[1] * 20))
+    quit_surface.fill(BUTTON_COLOR)
+    quit_surface.blit(quit_text, (10, 10))
+
+    #Init Button Rectangle
+    start_rectangle = start_surface.get_rect(center = (WIDTH // 2, HEIGHT // 2 + 50))
+    quit_rectangle = quit_surface.get_rect(center = (WIDTH // 2, HEIGHT // 2 + 150))
+
+    #Draw Buttons
+    screen.blit(start_surface, start_rectangle)
+    screen.blit(quit_surface, quit_rectangle)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if start_rectangle.collidepoint(event.pos):
+                    return
+                elif quit_rectangle.collidepoint(event.pos):
+                    sys.exit()
+        pygame.display.update()
+
+
+
+def main ():
+    game_over  = False
+
+    pygame.init()
